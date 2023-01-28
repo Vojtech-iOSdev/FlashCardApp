a709fe1a5c1fb26970be398ec902023ebe12944e
Vojtěch Kalivoda <kalivoda167@gmail.com>
2023-01-28 10:10:38 +0100

    Initial Commit

---
+++ FlashCardApp/FlashCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardAppApp.swift
+//  FlashCardApp
+//
+//  Created by Vojtěch Kalivoda on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
